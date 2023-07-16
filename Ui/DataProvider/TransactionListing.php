6d928a35723b0e1d42e54f2b367a81c2e5051a24
Imanuel Bertrand <imanuel@bertrand.one>
2023-07-16 15:31:30 +0200

    Fix transaction grid filters

--- Ui/DataProvider/TransactionListing.php
+++ Ui/DataProvider/TransactionListing.php
@@ -5,9 +5,17 @@ namespace Ibertrand\BankSync\Ui\DataProvider;
 use Exception;
 use Ibertrand\BankSync\Helper\Data;
 use Ibertrand\BankSync\Model\ResourceModel\Transaction\CollectionFactory;
+use Magento\Customer\Model\Customer;
+use Magento\Customer\Model\ResourceModel\Customer\CollectionFactory as CustomerCollectionFactory;
+use Magento\Framework\Api\Filter;
 use Magento\Framework\UrlInterface;
+use Magento\Sales\Model\Order;
 use Magento\Sales\Model\Order\CreditmemoRepository;
 use Magento\Sales\Model\Order\InvoiceRepository;
+use Magento\Sales\Model\ResourceModel\Order\Address\CollectionFactory as orderAddressCollectionFactory;
+use Magento\Sales\Model\ResourceModel\Order\CollectionFactory as OrderCollectionFactory;
+use Magento\Sales\Model\ResourceModel\Order\Creditmemo\CollectionFactory as CreditmemoCollectionFactory;
+use Magento\Sales\Model\ResourceModel\Order\Invoice\CollectionFactory as InvoiceCollectionFactory;
 use Magento\Ui\DataProvider\AbstractDataProvider;
 use Psr\Log\LoggerInterface;
 
@@ -18,6 +26,11 @@ class TransactionListing extends AbstractDataProvider
     protected InvoiceRepository $invoiceRepository;
     protected CreditmemoRepository $creditmemoRepository;
     protected LoggerInterface $logger;
+    protected InvoiceCollectionFactory $invoiceCollectionFactory;
+    protected CreditmemoCollectionFactory $creditmemoCollectionFactory;
+    protected OrderCollectionFactory $orderCollectionFactory;
+    protected CustomerCollectionFactory $customerCollectionFactory;
+    protected orderAddressCollectionFactory $orderAddressCollectionFactory;
 
     public function __construct(
         $name,
@@ -28,6 +41,11 @@ class TransactionListing extends AbstractDataProvider
         UrlInterface $urlBuilder,
         InvoiceRepository $invoiceRepository,
         CreditmemoRepository $creditmemoRepository,
+        InvoiceCollectionFactory $invoiceCollectionFactory,
+        CreditmemoCollectionFactory $creditmemoCollectionFactory,
+        OrderCollectionFactory $orderCollectionFactory,
+        CustomerCollectionFactory $customerCollectionFactory,
+        orderAddressCollectionFactory $orderAddressCollectionFactory,
         LoggerInterface $logger,
         array $meta = [],
         array $data = [],
@@ -37,6 +55,11 @@ class TransactionListing extends AbstractDataProvider
         $this->urlBuilder = $urlBuilder;
         $this->invoiceRepository = $invoiceRepository;
         $this->creditmemoRepository = $creditmemoRepository;
+        $this->invoiceCollectionFactory = $invoiceCollectionFactory;
+        $this->creditmemoCollectionFactory = $creditmemoCollectionFactory;
+        $this->orderCollectionFactory = $orderCollectionFactory;
+        $this->customerCollectionFactory = $customerCollectionFactory;
+        $this->orderAddressCollectionFactory = $orderAddressCollectionFactory;
         $this->logger = $logger;
         parent::__construct(
             $name,
@@ -81,5 +104,87 @@ class TransactionListing extends AbstractDataProvider
         }
 
         return $data;
+    }
+
+    /**
+     * @param Filter $filter
+     * @param int[]  $orderIds
+     */
+    protected function setFilterByOrderIds(Filter $filter, array $orderIds): void
+    {
+        $creditMemoIds = $this->creditmemoCollectionFactory->create()
+            ->addFieldToFilter('order_id', ['in' => $orderIds])
+            ->getAllIds();
+
+        $invoiceIds = $this->invoiceCollectionFactory->create()
+            ->addFieldToFilter('order_id', ['in' => $orderIds])
+            ->getAllIds();
+
+        $filter->setField('document_id')
+            ->setConditionType('in')
+            ->setValue(implode(',', array_merge($creditMemoIds, $invoiceIds)));
+    }
+
+    /**
+     * @param Filter $filter
+     *
+     * @return void
+     */
+    public function addFilter(Filter $filter)
+    {
+        if ($filter->getField() === 'order_increment_id') {
+            /** @var Order $order */
+
+            $orderIds = $this->orderCollectionFactory->create()
+                ->addFieldToFilter('increment_id', [$filter->getConditionType() => $filter->getValue()])
+                ->getAllIds();
+
+            $this->setFilterByOrderIds($filter, $orderIds);
+        }
+
+        if ($filter->getField() === 'document') {
+            /** @var Order $order */
+
+            $creditMemoIds = $this->creditmemoCollectionFactory->create()
+                ->addFieldToFilter('increment_id', [$filter->getConditionType() => $filter->getValue()])
+                ->getAllIds();
+            $invoiceIds = $this->invoiceCollectionFactory->create()
+                ->addFieldToFilter('increment_id', [$filter->getConditionType() => $filter->getValue()])
+                ->getAllIds();
+
+            $filter->setField('document_id')
+                ->setConditionType('in')
+                ->setValue(implode(',', array_merge($creditMemoIds, $invoiceIds)));
+        }
+
+        if ($filter->getField() === 'customer_increment_id') {
+            /** @var Customer $customer */
+
+            $customer = $this->customerCollectionFactory->create()
+                ->addFieldToFilter('increment_id', [$filter->getConditionType() => $filter->getValue()])
+                ->getFirstItem();
+
+            $orderIds = $this->orderCollectionFactory->create()
+                ->addFieldToFilter('customer_id', $customer->getId())
+                ->getAllIds();
+
+            $this->setFilterByOrderIds($filter, $orderIds);
+        }
+
+        if ($filter->getField() == 'document_name') {
+            $orderIds = $this->orderAddressCollectionFactory->create()
+                ->addFieldToFilter(
+                    ['firstname', 'lastname', 'company'],
+                    [
+                        [$filter->getConditionType() => $filter->getValue()],
+                        [$filter->getConditionType() => $filter->getValue()],
+                        [$filter->getConditionType() => $filter->getValue()],
+                    ]
+                )
+                ->getColumnValues('parent_id');
+            $this->setFilterByOrderIds($filter, $orderIds);
+        }
+
+        parent::addFilter($filter);
     }
 }
