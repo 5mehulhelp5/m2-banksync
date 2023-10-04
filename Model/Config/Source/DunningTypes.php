a818b48fe548a94181161a56803d5885abec4610
Imanuel Bertrand <imanuel@bertrand.one>
2023-10-04 14:37:11 +0200

    Dunning column changes

---
+++ Model/Config/Source/DunningTypes.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Ibertrand\BankSync\Model\Config\Source;
+
+use Ibertrand\BankSync\Helper\Dunning;
+use Magento\Framework\Data\OptionSourceInterface;
+
+class DunningTypes implements OptionSourceInterface
+{
+    protected Dunning $dunningHelper;
+
+    /**
+     * @param Dunning $dunningHelper
+     */
+    public function __construct(Dunning $dunningHelper)
+    {
+        $this->dunningHelper = $dunningHelper;
+    }
+
+    /**
+     * @return array|array[]
+     */
+    public function toOptionArray()
+    {
+        $storeId = 0;
+        return array_map(
+            fn ($type) => ['value' => $type, 'label' => $this->dunningHelper->getTypeLabel($type, $storeId)],
+            $this->dunningHelper->getEnabledDunningTypes($storeId),
+        );
+    }
+
+    /**
+     * @return string[]
+     */
+    public function toArray(): array
+    {
+        $storeId = 0;
+        $result = [];
+        foreach ($this->dunningHelper->getEnabledDunningTypes(0) as $type) {
+            $result[$type] = $this->dunningHelper->getTypeLabel($type, $storeId);
+        }
+        return $result;
+    }
+}
--- Ui/DataProvider/DunningListing.php
+++ Ui/DataProvider/DunningListing.php
@@ -8,14 +8,14 @@ use Ibertrand\BankSync\Logger\Logger;
 use Ibertrand\BankSync\Model\ResourceModel\Dunning\CollectionFactory;
 use Magento\Customer\Model\ResourceModel\Customer\CollectionFactory as CustomerCollectionFactory;
 use Magento\Framework\Api\Filter;
+use Magento\Framework\Exception\InputException;
+use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Framework\UrlInterface;
 use Magento\Sales\Model\Order\InvoiceRepository;
 use Magento\Sales\Model\ResourceModel\Order\Address\CollectionFactory as OrderAddressCollectionFactory;
 use Magento\Sales\Model\ResourceModel\Order\CollectionFactory as OrderCollectionFactory;
 use Magento\Sales\Model\ResourceModel\Order\Invoice\CollectionFactory as InvoiceCollectionFactory;
 use Magento\Ui\DataProvider\AbstractDataProvider;
-use Zend_Db;
-use Zend_Db_Select;
 
 class DunningListing extends AbstractDataProvider
 {
@@ -49,6 +49,9 @@ class DunningListing extends AbstractDataProvider
         array $data = [],
     ) {
         $this->collection = $collectionFactory->create();
+        $this->collection->join(['invoice' => 'sales_invoice'], 'invoice.entity_id = main_table.invoice_id', []);
+        $this->collection->join(['order' => 'sales_order'], 'order.entity_id = invoice.order_id', []);
+
         $this->config = $config;
         $this->display = $display;
         $this->urlBuilder = $urlBuilder;
@@ -69,23 +72,38 @@ class DunningListing extends AbstractDataProvider
         );
     }
 
+    /**
+     * @return array
+     * @throws InputException
+     * @throws NoSuchEntityException
+     */
     public function getData()
     {
         $data = parent::getData();
 
         foreach ($data['items'] as $key => $item) {
             $invoice = $this->invoiceRepository->get($item['invoice_id']);
-            $item['invoice_increment_id'] = $this->display->getObjectLink($invoice);
-            $item['dunning_type'] = $this->config->getDunningTypeLabel($item['dunning_type'], $invoice->getStoreId());
-            $item['email_address'] = $invoice->getOrder()->getCustomerEmail();
-            $item['is_sent'] = (int)!empty($item['sent_at']);
-
-            $data['items'][$key] = $item;
+            $data['items'][$key] = array_replace($item, [
+                'email_address' => $invoice->getOrder()->getCustomerEmail(),
+                'invoice_date' => $invoice->getCreatedAt(),
+                'invoice_increment_id' => $this->display->getObjectLink($invoice),
+                'is_sent' => (int)!empty($item['sent_at']),
+            ]);
         }
 
         return $data;
     }
 
+    /**
+     * @param Filter $filter
+     * @return void
+     */
+    protected function setFilterIsSent(Filter $filter): void
+    {
+        $filter->setField('sent_at')
+            ->setConditionType($filter->getValue() ? 'notnull' : 'null')
+            ->setValue(true);
+    }
 
     /**
      * @param Filter $filter
@@ -94,39 +112,18 @@ class DunningListing extends AbstractDataProvider
      */
     public function addFilter(Filter $filter)
     {
-        if ($filter->getField() === 'is_sent') {
-            $filter->setField('sent_at')
-                ->setConditionType($filter->getValue() ? 'notnull' : 'null')
-                ->setValue(true);
-        }
+        $processors = [
+            'email_address' => 'order.customer_email',
+            'invoice_date' => 'invoice.created_at',
+            'invoice_increment_id' => 'invoice.increment_id',
+            'is_sent' => [$this, 'setFilterIsSent'],
+        ];
 
-        if ($filter->getField() === 'invoice_increment_id') {
-            $invoiceCollection = $this->invoiceCollectionFactory->create()
-                ->addFieldToFilter('increment_id', [$filter->getConditionType() => $filter->getValue()])
-                ->getAllIds();
-            $filter->setField('invoice_id')
-                ->setConditionType('in')
-                ->setValue(implode(',', $invoiceCollection));
-        }
-
-        if ($filter->getField() === 'email_address') {
-            $relevantInvoiceIds = $this->dunningCollectionFactory->create()
-                ->join(['invoice' => 'sales_invoice'], 'invoice.entity_id = main_table.invoice_id', [])
-                ->getSelect()
-                ->reset(Zend_Db_Select::COLUMNS)
-                ->columns('invoice.entity_id')
-                ->query()
-                ->fetchAll(Zend_Db::FETCH_COLUMN);
-            $invoiceCollection = $this->invoiceCollectionFactory->create()
-                ->join(['order' => 'sales_order'], 'order.entity_id = main_table.order_id', [])
-                ->addFieldToFilter('main_table.entity_id', ['in' => $relevantInvoiceIds])
-                ->addFieldToFilter('order.customer_email', [$filter->getConditionType() => $filter->getValue()]);
-            $this->logger->info($invoiceCollection->getSelect()->__toString());
-            $invoiceCollection = $invoiceCollection->getAllIds();
-
-            $filter->setField('invoice_id')
-                ->setConditionType('in')
-                ->setValue(implode(',', $invoiceCollection));
+        $processor = $processors[$filter->getField()] ?? null;
+        if (is_callable($processor)) {
+            $processor($filter);
+        } elseif (is_string($processor)) {
+            $filter->setField($processor);
         }
 
         parent::addFilter($filter);
@@ -139,12 +136,17 @@ class DunningListing extends AbstractDataProvider
      */
     public function addOrder($field, $direction)
     {
-        if ($field === 'invoice_increment_id') {
-            $field = 'invoice_id';
+        $changes = [
+            'email_address' => 'order.customer_email',
+            'invoice_date' => 'invoice.created_at',
+            'invoice_increment_id' => 'invoice.increment_id',
+            'is_sent' => 'sent_at',
+        ];
+
+        if (isset($changes[$field])) {
+            $field = $changes[$field];
         }
-        if ($field === 'is_sent') {
-            $field = 'sent_at';
-        }
+
         parent::addOrder($field, $direction);
     }
 }
